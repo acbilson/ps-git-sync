7e6c85b3e82c28058347194824b941d13038b911
Alex Bilson <BilsonA@aimspecialtyhealth.com>
2019-11-15 13:46:38 -0600

    adjusts header in case of buffer width change

--- GitUtil.ps1
+++ GitUtil.ps1
@@ -4,15 +4,19 @@ param(
 )
 
 	# Sets the spacing width for console output
-	$consoleWidth = $Host.UI.RawUI.WindowSize.Width / 3
+	$consoleWidth = [math]::Floor($Host.UI.RawUI.WindowSize.Width / 3)
 	
 	# Sets the width of the header buffer
 	$leftBufferWidth = [math]::Floor(($consoleWidth - $Header.Length) / 2)
 	$rightBufferWidth = $leftBufferWidth
 	
 	# adjusts buffer width for odd header names to keep them the same length as even
-	if ($Header.Length % 2 -eq 0) {
+	if ($consoleWidth % 2 -eq 1 -and $Header.Length % 2 -eq 0) 
+	{
 		$leftBufferWidth = $leftBufferWidth + 1
+	} elseif ($consoleWidth % 2 -eq 0 -and $Header.Length % 2 -eq 0)
+	{
+		$leftBufferWidth = $leftBufferWidth - 1
 	}
 	
 	# Write left buffer
